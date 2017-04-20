cea3ede882620c5b1d4bc49def62357ba5c9efbe
Amos Garner <aagarner.7@gmail.com>
2017-04-20 11:25:40 -0400

    Init inputvalidator script

---
+++ inputValidator.py
@@ -0,0 +1,12 @@
+alphaNumArray = [alphaArray, numArray, specialArray]
+def validateInput(input):
+    return True
+
+def checkInAlpha(input):
+    return True
+
+def checkInNum(input):
+    return True
+
+def checkInSpecial(input):
+    return True

99b5fc761d464af20166dc4b263bda063fb5436d
Amos Garner <aagarner.7@gmail.com>
2017-04-20 13:36:40 -0400

    Implement data validation checker

--- inputValidator.py
+++ inputValidator.py
@@ -1,18 +1,16 @@
 from alphaNumLib import *
 
-alphaNumArray = [alphaArray, numArray, specialArray]
+alphaNumArray = alphaArray + numArray + specialArray
 
 def validateInput(input):
-    if(checkInAlpha and checkInNum and checkInSpecial):
+    if(checkInAlphaNumSpec(input)):
         return True
     else:
         return False
 
-def checkInAlpha(input):
-    return True
-
-def checkInNum(input):
-    return True
-
-def checkInSpecial(input):
+def checkInAlphaNumSpec(input):
+    inputCharArray = list(input.lower())
+    for value in inputCharArray:
+        if value not in alphaNumArray:
+            return False
     return True
